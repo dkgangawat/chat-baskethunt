240dcc8a6a5739605736c3ce28b2afe86a429b1c
yaseen <yaseen.ragib@gmaail.com>
2023-06-15 00:33:18 +0530

    edit user

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { admin, db, storage } = require('../firebase');
 const bcrypt = require('bcrypt');
 
-
 const usersCollection = db.collection('users');
 
 // Serve the admin.html file for the root route
@@ -23,45 +22,66 @@ router.post('/users', async(req, res) => {
         res.status(500).json({ error: 'Error fetching users' });
     }
 });
+
 const hashPass = async(pass) => {
     const saltRounds = 10;
     const encryptedPass = await bcrypt.hash(pass, saltRounds)
     return encryptedPass;
-
 }
-router.post('/adduser', async(req, res) => {
-    const { username, email, password } = req.body;
+
+router.post("/adduser", async (req, res) => {
+    const { username, email, password, photoURL = "" } = req.body;
+  
+    try {
+      const usersCollection = db.collection("users");
+      const snapshot = await usersCollection.get();
+      const userCount = snapshot.size;
+  
+      // Insert sample users
+      const hasPass = await hashPass(password);
+      const newuser = {
+        username,
+        password: hasPass,
+        email,
+        photoURL,
+      };
+  
+      const batch = db.batch();
+  
+      const userRef = usersCollection.doc();
+      batch.set(userRef, { ...newuser, id: userRef.id });
+  
+      await batch.commit();
+      console.log("Sample users inserted successfully");
+  
+      res.send();
+    } catch (error) {
+      console.error("Error inserting sample users:", error);
+      res.status(500).json({ error: "Error inserting sample users" });
+    }
+  });
+  
+router.put('/users/:id', async(req, res) => {
+    const userId = req.params.id;
+    const { username, email } = req.body;
 
     try {
         const usersCollection = db.collection('users');
-        const snapshot = await usersCollection.get();
-        const userCount = snapshot.size;
+        const userDoc = usersCollection.doc(userId);
+        const userSnapshot = await userDoc.get();
 
-        // Insert sample users
-        const hasPass = await hashPass(password);
-        const newuser = {
-            username,
-            password: hasPass,
-            email,
-            photoURL: ''
-        };
+        if (!userSnapshot.exists) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
 
-        const batch = db.batch();
-
-        const userRef = usersCollection.doc();
-        batch.set(userRef, {...newuser, id: userRef.id });
-
-        await batch.commit();
-        console.log('Sample users inserted successfully');
-
-
-        res.send();
+        await userDoc.update({ username, email });
+        res.sendStatus(200);
     } catch (error) {
-        console.error('Error inserting sample users:', error);
-        res.status(500).json({ error: 'Error inserting sample users' });
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: 'Error updating user' });
     }
 });
-
 
 router.delete('/users/:id', async(req, res) => {
     const userId = req.params.id;
@@ -84,6 +104,5 @@ router.delete('/users/:id', async(req, res) => {
         res.status(500).json({ error: 'Error deleting user' });
     }
 });
-
 
 module.exports = router;
